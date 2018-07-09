97908b4a1591bba721f7d1becd0a31310931947d
Katherine Arackaparambil <katherineara@fb.com>
2018-07-09 15:56:47 -0700

    First blank post

--- app/src/main/java/codepath/com/instagram/HomeActivity.java
+++ app/src/main/java/codepath/com/instagram/HomeActivity.java
@@ -1,11 +1,16 @@
 package codepath.com.instagram;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
+import codepath.com.instagram.model.Post;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,17 +18,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ParseQuery.getQuery(Post.class).findInBackground(new FindCallback<Post>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); ++i) {
+                        Log.d("HomeActivity", "Post(" + i + ") " + objects.get(i).getDescription());
+                    }
+
+                } else {
+                    e.printStackTrace();
+                }
             }
         });
+
     }
 
 }
--- app/src/main/java/codepath/com/instagram/ParseApp.java
+++ app/src/main/java/codepath/com/instagram/ParseApp.java
@@ -3,11 +3,17 @@ package codepath.com.instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
+
+import codepath.com.instagram.model.Post;
 
 public class ParseApp extends Application {
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
                 .applicationId("katherine-fbu-instagram")

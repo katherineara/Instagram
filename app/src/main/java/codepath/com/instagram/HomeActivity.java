444254aadfcfd9028c4d9ae0738084a20fca2128
Katherine Arackaparambil <katherineara@fb.com>
2018-07-10 14:21:09 -0700

    Video 5

--- app/src/main/java/codepath/com/instagram/HomeActivity.java
+++ app/src/main/java/codepath/com/instagram/HomeActivity.java
@@ -1,23 +1,103 @@
 package codepath.com.instagram;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.List;
 
 import codepath.com.instagram.model.Post;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String imagePath = Environment.getExternalStorageDirectory() + "/Desktop/beach.jpg";
+    private EditText descriptionInput;
+    private Button createButton;
+    private Button refreshButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        descriptionInput = findViewById(R.id.description_et);
+        createButton = findViewById(R.id.create_btn);
+        refreshButton = findViewById(R.id.refresh_btn);
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String description = descriptionInput.getText().toString();
+                final ParseUser user = ParseUser.getCurrentUser();
+
+                final File file = new File(imagePath);
+                final ParseFile parseFile = new ParseFile(file);
+
+                createPost(description, parseFile, user);
+            }
+        });
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadTopPosts();
+            }
+        });
+
+        final Post.Query postsQuery = new Post.Query();
+        postsQuery.getTop().withUser();
+
+        postsQuery.getQuery(Post.class).findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); ++i) {
+                        try {
+                            Log.d("HomeActivity", "Post(" + i + ") " +
+                                    objects.get(i).getDescription() + "\nusername = "
+                                    + objects.get(i).getUser().fetchIfNeeded().getUsername()
+                            );
+                        } catch (ParseException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void createPost(String description, ParseFile imageFile,ParseUser user) {
+        final Post newPost = new Post();
+        newPost.setDescription(description);
+        newPost.setImage(imageFile);
+        newPost.setUser(user);
+
+        newPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("HomeActivity", "Create post success");
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void loadTopPosts() {
         final Post.Query postsQuery = new Post.Query();
         postsQuery.getTop().withUser();
 

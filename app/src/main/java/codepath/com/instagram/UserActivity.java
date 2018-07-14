9287c12d444551718ac8c84241a43d35937ee8c4
Katherine Arackaparambil <katherineara@fb.com>
2018-07-13 19:48:36 -0700

    Added user profile view

--- app/src/main/java/codepath/com/instagram/UserActivity.java
+++ app/src/main/java/codepath/com/instagram/UserActivity.java
@@ -1,13 +1,41 @@
 package codepath.com.instagram;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.parse.ParseFile;
+
+import org.parceler.Parcels;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import codepath.com.instagram.model.Post;
 
 public class UserActivity extends AppCompatActivity {
+
+    Post post;
+    @BindView(R.id.tvUsername) public TextView tvUsername;
+    @BindView(R.id.ivProfile) public ImageView ivProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        ButterKnife.bind(this);
+
+        post = (Post) Parcels.unwrap(getIntent().getParcelableExtra(Post.class.getSimpleName()));
+
+        tvUsername.setText(post.getUser().getUsername().toString());
+        ParseFile profileImage = post.getUser().getParseFile("profilePic");
+        if (profileImage != null) {
+            Glide.with(this)
+                    .load(profileImage.getUrl())
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(ivProfile);
+        }
     }
 }
